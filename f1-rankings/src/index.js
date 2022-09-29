fd81d38ad11fb508077fa05c3e12613137b71c8f
Matthew Van Rooyen <46861609+MpVrooyeN@users.noreply.github.com>
2022-09-29 14:15:13 +0200

    Installed jQuery

--- f1-rankings/src/index.js
+++ f1-rankings/src/index.js
@@ -1,7 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
+import $ from 'jquery';
 
-const myFirstElement = <h1>Hello React!</h1>
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
