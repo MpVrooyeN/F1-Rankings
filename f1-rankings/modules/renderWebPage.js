966d74d6d70cac587fee1452f959d0cf38eebb9f
Matthew Van Rooyen <46861609+MpVrooyeN@users.noreply.github.com>
2022-10-01 17:43:08 +0200

    Added a react-table

--- f1-rankings/modules/renderWebPage.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-function RenderTitle(titleString) {
-    return ReactDOM.createRoot(document.getElementById('root')).render(React.createElement('h1', {}, titleString));
-}
--- f1-rankings/src/index.js
+++ f1-rankings/src/index.js
@@ -1,3 +1,19 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import $ from 'jquery';
+
+function renderTitle(titleString) {
+    ReactDOM.createRoot(document.getElementById('root')).render(React.createElement('h1', {}, titleString));
+}
+
+function renderTable() {
+    let table = document.createElement('table')
+    ReactDOM.createRoot(document.getElementById('root')).render(React.createElement(table))
+}
+
+function RenderWebPage() {
+    renderTable();
+    renderTitle("F1 Rankings");
+}
+
+RenderWebPage();
