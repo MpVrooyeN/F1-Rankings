91a98c1d8292ade84e3c85be5416e0975523ad5f
Matthew Van Rooyen <46861609+MpVrooyeN@users.noreply.github.com>
2022-10-03 19:19:49 +0200

    Switching to functional programming Changed styling

--- f1-rankings/src/Table.js
+++ f1-rankings/src/Table.js
@@ -9,8 +9,8 @@ class ReactTable extends Component {
             return document.getElementById("root");
         }
 
-        function getJSONKeys() {
-            return Object.keys(data[0])
+        function getJSONKeys(JSON, key = 0) {
+            return Object.keys(JSON[key])
         }
 
         function createTableBody() {
@@ -26,7 +26,7 @@ class ReactTable extends Component {
 
         function getHeading() {
             let heading = document.createElement('th')
-            heading.setAttribute('colspan', getJSONKeys().length)
+            heading.setAttribute('colspan', getJSONKeys(data).length)
             heading.setAttribute('background-color', 'red')
             let h1 = document.createElement('h1')
             h1.appendChild(document.createTextNode('F1-Rankings'))
@@ -36,17 +36,22 @@ class ReactTable extends Component {
 
         function createTableHeaders() {
             let thr2 = document.createElement('tr');
-            for (let element of getJSONKeys()) {
-                if (element === getJSONKeys()[1]) {
-                    console.log("skipped")
+            for (let element of getJSONKeys(data)) {
+                if (element === getJSONKeys(data)[1]) {
+                    console.log("Skipped PositionText Header")
                 } else {
                     let th2 = document.createElement('th')
-                    if (element === getJSONKeys()[0]) {
-                        element = 'Pos'
-                        th2.setAttribute('width', '20%')
-                    }
                     let h2 = document.createElement('h3');
-                    h2.appendChild(document.createTextNode(element.charAt(0).toUpperCase() + element.slice(1).toLowerCase()));
+                    if (element === getJSONKeys(data)[0]) {
+                        h2.appendChild(document.createTextNode('Pos'));
+                        th2.setAttribute('width', '20%')
+                    } else if (element === getJSONKeys(data)[2]) {
+                        h2.appendChild(document.createTextNode('Pts'));
+                        th2.setAttribute('width', '20%')
+                    } else {
+                        h2.appendChild(document.createTextNode(element.charAt(0).toUpperCase() + element.slice(1).toLowerCase()));
+                    }
+
                     th2.appendChild(h2)
                     thr2.appendChild(th2)
                 }
@@ -55,30 +60,30 @@ class ReactTable extends Component {
             return thr2
         }
 
+        function tableDataPerRow(tableRow, rowData, indexRow, indexData) {
+            if (indexData === "Constructor") {
+                rowData.appendChild(document.createTextNode(JSON.stringify(data[indexRow][indexData]['name']).slice(1, -1)));
+                rowData.setAttribute('width', '100%')
+            } else if (indexData === "wins") {
+                if (JSON.stringify(data[indexRow][indexData]).slice(1, -1) === "0") {
+                    rowData.appendChild(document.createTextNode('-'));
+                } else {
+                    rowData.appendChild(document.createTextNode(JSON.stringify(data[indexRow][indexData]).slice(1, -1)));
+                }
+            } else {
+                rowData.appendChild(document.createTextNode(JSON.stringify(data[indexRow][indexData]).slice(1, -1)));
+            }
+            return tableRow.appendChild(rowData)
+        }
 
         function createTableData() {
             let returnV = []
             for (let k in data) {
                 let tr = document.createElement('tr');
                 for (let j in data[k]) {
-                    if (j === "positionText") {
-                        console.log("skipped")
-                    } else if (j === "Constructor") {
-                        let td = document.createElement('td');
-                        td.appendChild(document.createTextNode(JSON.stringify(data[k][j]['name']).slice(1, -1)));
-                        td.setAttribute('width', '100%')
-                        tr.appendChild(td);
-                    } else if (j === "Wins") {
-                        console.log(data[k][j])
-                        if (data[k][j] === 0) {
-                            let td = document.createElement('td');
-                            td.appendChild(document.createTextNode('-'));
-                            tr.appendChild(td);
-                        }
-                    } else {
-                        let td = document.createElement('td');
-                        td.appendChild(document.createTextNode(JSON.stringify(data[k][j]).slice(1, -1)));
-                        tr.appendChild(td);
+                    let td = document.createElement('td');
+                    if (j !== "positionText") {
+                        tableDataPerRow(tr, td, k, j)
                     }
                 }
                 returnV.push(tr)
@@ -87,7 +92,6 @@ class ReactTable extends Component {
         }
         return createTable().appendChild(createTableBody());
     }
-
 }
 
 export default ReactTable 
