ac06a5907766075790ec3ec5d275a6db2ff630aa
Matthew Van Rooyen <46861609+MpVrooyeN@users.noreply.github.com>
2022-10-07 19:59:38 +0200

    Optimized code

--- src/components/mui/PageContainer.js
+++ src/components/mui/PageContainer.js
@@ -3,86 +3,62 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider, ToggleButton, ToggleButtonGroup } from '@mui/material';
-import Standings from "./../Standings/Standings";
+import { ThemeProvider, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import { useConstuctorStandings, useDriverStandings } from '../../queries/standing.query';
-import { useState } from 'react';
-
-const customTheme = createTheme({
-	palette: {
-		mode: 'light',
-		primary: {
-			main: '#e22420',
-			contrastText: '#000'
-		},
-		secondary: {
-			main: '#f50057',
-		},
-	},
-	typography: {
-		fontFamily: 'Titllium',
-	},
-});
-
-
-
+import { useState, useMemo, Fragment } from 'react';
+import Standings from '../Standings/Standings';
+import { ConstructorColumns as constructorCol, DriverColumns as driverCol } from '../Table/Columns'
+import { CustomTheme as customTheme } from './CustomTheme';
 
 export default function PageContainer() {
-	const [table, setTable] = useState('constructor');
-	const constructorData = useConstuctorStandings();
-	const [headingText, setHeadingText] = useState("F1 Team Standings")
-	const constructorCol = [
-		{ Header: 'Team Name', accessor: 'Constructor.name' },
-		{ Header: 'Position', accessor: 'position' },
-		{ Header: 'Points', accessor: 'points' },
-		{ Header: 'Wins', accessor: 'wins' }]
-	const driverData = useDriverStandings();
-	const driverCol = [
-		{	Header: 'Driver Name', accessor: 'Driver.name'},
-		{	Header: 'Position',	accessor: 'position'},
-		{	Header: 'Points',	accessor: 'points'},
-		{	Header: 'Wins',	accessor: 'wins'}]
-	const [data, setData] = useState(constructorData);
-	const [columns, setColumns] = useState(constructorCol)
-	const useToggle = () => {
-		const x = document.getElementById("heading");
+	const { data: constructorData, isLoading: isConstructorLoading } = useConstuctorStandings();
+	const { data: driverData, isLoading: isDriverLoading } = useDriverStandings();
+	const isLoading = isConstructorLoading && isDriverLoading
+	const [basicTable, setBasicTable] = useState([]);
+	const [{table, headingText}, setState] = useState({table: 'constructor', headingText: 'F1 Team Standings'});
+	useMemo(() => {
 		if (table === 'constructor') {
-			x.innerHTML = "Driver Standings"
-			setTable('driver');
-			setData(driverData);
-			setColumns(driverCol)
+			setBasicTable([{ tableData: constructorData, col: constructorCol }]);
 		} else {
-			x.innerHTML = "Constructor Standings"
-			setTable('constructor')
-			setData(constructorData);
-			setColumns(constructorCol);
+			setBasicTable([{ tableData: driverData, col: driverCol }]);
 		}
-	}
+	}, [table, constructorData, driverData])
 
-	return (<>
-		<ThemeProvider theme={customTheme}>
-			<Box sx={{ flexGrow: 1 }}>
-				<AppBar position="static" color={"primary"}>
-					<Toolbar>
-						<Typography variant="h6" id="heading" component="div" fontWeight={1000} sx={{ flexGrow: 1 }}>
-							F1 Standings
-						</Typography>
-						<ToggleButtonGroup
-							value={table}
-							exclusive
-							onChange={useToggle}
-							aria-label="text alignment">
-							<ToggleButton value={'constructor'}>Constructor
-							</ToggleButton>
-							<ToggleButton value={'driver'}>Driver
-							</ToggleButton>
-						</ToggleButtonGroup>
-					</Toolbar>
-				</AppBar>
-			</Box>
-		</ThemeProvider>
-		<Standings tableData={data} col={columns} />
-	</>
+	//Handles the toggle button group
+	const handleToggle = () => {
+		setState({
+			table: table === 'driver' ? 'constructor' : 'driver',
+			headingText: table === 'driver' ? "F1 Team Standings" : "F1 Driver Standings"
+		});
+	};
+
+	return (
+		<Fragment>
+			<ThemeProvider theme={customTheme}>
+				<Box sx={{ flexGrow: 1 }}>
+					<AppBar position="static" color={"primary"}>
+						<Toolbar>
+							<Typography variant="h6" id="heading" component="div" fontWeight={1000} sx={{ flexGrow: 1 }}>
+								{headingText}
+							</Typography>
+							<ToggleButtonGroup
+								value={table}
+								exclusive
+								onChange={handleToggle}
+								aria-label="text alignment">
+								<ToggleButton value={'constructor'}>Constructor
+								</ToggleButton>
+								<ToggleButton value={'driver'}>Driver
+								</ToggleButton>
+							</ToggleButtonGroup>
+						</Toolbar>
+					</AppBar>
+				</Box>
+			</ThemeProvider>
+			{isLoading && <p>Loading</p>}
+			{!isLoading && constructorData && driverData && (
+				<Standings {...basicTable} />
+			)}
+		</Fragment>
 	);
 }
-
