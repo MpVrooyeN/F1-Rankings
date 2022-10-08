30103f49b2b89a08477605b7cc535e80bcc56db6
Matthew Van Rooyen <46861609+MpVrooyeN@users.noreply.github.com>
2022-10-08 20:00:23 +0200

    removed redundancies

--- src/components/Standings/Standings.js
+++ src/components/Standings/Standings.js
@@ -1,21 +1,14 @@
-import { useConstuctorStandings } from "../../queries/standing.query";
-import { BasicTable } from "../Table/BasicTable";
+import BasicTable from "../Table/BasicTable";
 import React from "react";
 
-const Standings = () => {
+const Standings = (props) => {
   // react query
   // data is what comes back from API
   // isLoading is the state of the API request
   // the brackets wrapping data etc. is called destructuring
-  const {data, isLoading} = useConstuctorStandings();
   return (
     <>
-      
-      {/* ternary operator */}
-      {isLoading && <p>Loading</p>}
-      {!isLoading && data && (
-        <BasicTable />
-      )}
+      <BasicTable {...props}/>
     </>);
 };
 
