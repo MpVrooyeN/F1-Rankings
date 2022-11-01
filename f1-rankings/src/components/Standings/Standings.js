4e91dd7dd248e4df0d165d837731dc34076a12f4
Matthew Van Rooyen <46861609+MpVrooyeN@users.noreply.github.com>
2022-11-02 00:15:52 +0200

    Had to install React-Dom, Axios and Tanstack Inserted a react table instead of hard HTML Styled react table with CSS file

--- f1-rankings/src/components/Standings/Standings.js
+++ f1-rankings/src/components/Standings/Standings.js
@@ -1,41 +1,21 @@
 import { useStandings } from "../../queries/standing.query";
+import { BasicTable } from "../Table/BasicTable";
 
 const Standings = () => {
   // react query
   // data is what comes back from API
   // isLoading is the state of the API request
   // the brackets wrapping data etc. is called destructuring
-  const { data, isLoading } = useStandings();
-
+  const {data, isLoading} = useStandings();
   return (
     <>
       <h1>Rankings</h1>
       {/* ternary operator */}
       {isLoading && <p>Loading</p>}
       {!isLoading && data && (
-        <table style={{ width: "100%" }}>
-          <thead>
-            <tr style={{ textAlign: "left" }}>
-              <th>Team name</th>
-              <th>Position</th>
-              <th>Points</th>
-              <th>Wins</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => (
-              <tr key={JSON.stringify(item)}>
-                <td>{item?.Constructor?.name}</td>
-                <td>{item?.position}</td>
-                <td>{item?.points}</td>
-                <td>{item?.wins}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <BasicTable />
       )}
-    </>
-  );
+    </>);
 };
 
 export default Standings;
---
+++ f1-rankings/src/components/Table/Columns.js
@@ -0,0 +1,19 @@
+export const COLUMNS = 
+    [
+        {
+            Header: 'Team Name',
+            accessor: 'Constructor.name'
+        },
+        {
+            Header: 'Position',
+            accessor: 'position'
+        },
+        {
+            Header: 'Points',
+            accessor: 'points'
+        },
+        {
+            Header: 'Wins',
+            accessor: 'wins'
+        }
+    ]
